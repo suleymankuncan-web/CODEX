408001873f6ec820d037c36c8bb46abe3e86c21a
suleymankuncan-web <suleymankuncan@gmail.com>
2026-05-19 14:06:11 +0300

    feat: generate personnel master list types (#286)

--- admin-web/scripts/generate-openapi-types.mjs
+++ admin-web/scripts/generate-openapi-types.mjs
@@ -15,6 +15,7 @@ const httpMethods = new Set(['get', 'post', 'put', 'patch', 'delete'])
 const selectedOperations = [
   { path: '/api/integrations/import-batches/overview', method: 'get' },
   { path: '/api/integrations/lookups', method: 'get' },
+  { path: '/api/integrations/personnel-master', method: 'get' },
   { path: '/api/integrations/personnel-master-lookups', method: 'get' },
   { path: '/api/integrations/store-master', method: 'get' },
   { path: '/api/integrations/store-master-lookups', method: 'get' },
